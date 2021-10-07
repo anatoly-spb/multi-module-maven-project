cd68efaa44f30ab2d46646b8851ece99affdb376
Anatoly Shirokov <anatoliy.shirokov@globalports.com>
2021-10-07 15:33:38 +0300

    Add pom.xml, module1, module2

---
+++ module1/src/main/java/ru/rlisystems/Module1.java
@@ -0,0 +1,13 @@
+package ru.rlisystems;
+
+/**
+ * Hello world!
+ *
+ */
+public class Module1
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
